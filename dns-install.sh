cabc284950f01df633dcf7023ce163d193cd860f
Angristan <stanislas.lange@openmailbox.org>
2015-07-06 10:55:24 +0200

    Added some comments

--- dns-install.sh
+++ dns-install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if [ "$UID" -ne "0" ]
+if [ "$UID" -ne "0" ] #User check
 then
    echo -e "Use this script as root."
    exit
 else
 pacman -Sy bind dnsutils
-wget ftp://ftp.internic.net/domain/named.cache -O /var/named/root.hint
+wget ftp://ftp.internic.net/domain/named.cache -O /var/named/root.hint #Root servers list
 sed -i 's|file "/var/named/root.hint"|file "root.hint"|' /etc/named.conf
-sudo chattr -i /etc/resolv.conf
+sudo chattr -i /etc/resolv.conf #Allow the modification of the file
 sed -i 's|nameserver|#nameserver|' /etc/resolv.conf
-echo "nameserver 127.0.0.1" >> /etc/resolv.conf
-sudo chattr +i /etc/resolv.conf
-sudo systemctl start named && sudo systemctl enable named
+echo "nameserver 127.0.0.1" >> /etc/resolv.conf #Set localhost as the DNS resolver
+sudo chattr +i /etc/resolv.conf #Disallow the modification of the file
+sudo systemctl start named && sudo systemctl enable named #Enable named at boot and start it
 fi

a603b1464bf29ac193e16c42e4738c8b6295fc07
Angristan <stanislas.lange@openmailbox.org>
2015-08-15 18:54:14 +0200

    Update unbound-install.sh

--- unbound-install.sh
+++ unbound-install.sh
@@ -2,18 +2,18 @@
 
 if [ "$UID" -ne "0" ] #User check
 then
-echo -e "Use this script as root."
-exit
+  echo "Use this script as root."
+  exit 1
 else
-pacman -Sy unbound expat
-wget ftp://FTP.INTERNIC.NET/domain/named.cache -O /etc/unbound/root.hints #Root servers list
-chmod 666 /etc/unbound/unbound.conf
-echo "  root-hints: "/etc/unbound/root.hints"" >> /etc/unbound/unbound.conf
-echo "  interface: 127.0.0.1" >> /etc/unbound/unbound.conf
-chmod 644 /etc/unbound/unbound.conf
-chattr -i /etc/resolv.conf #Allow the modification of the file
-sed -i 's|nameserver|#nameserver|' /etc/resolv.conf #Disable previous DNS servers
-echo "nameserver 127.0.0.1" >> /etc/resolv.conf #Set localhost as the DNS resolver
-chattr +i /etc/resolv.conf #Disallow the modification of the file
-systemctl start unbound && systemctl enable unbound #Enable named at boot and start it
+  pacman -Sy unbound expat
+  wget ftp://FTP.INTERNIC.NET/domain/named.cache -O /etc/unbound/root.hints #Root servers list
+  chmod 666 /etc/unbound/unbound.conf
+  echo "  root-hints: "/etc/unbound/root.hints"" >> /etc/unbound/unbound.conf
+  echo "  interface: 127.0.0.1" >> /etc/unbound/unbound.conf
+  chmod 644 /etc/unbound/unbound.conf
+  chattr -i /etc/resolv.conf #Allow the modification of the file
+  sed -i 's|nameserver|#nameserver|' /etc/resolv.conf #Disable previous DNS servers
+  echo "nameserver 127.0.0.1" >> /etc/resolv.conf #Set localhost as the DNS resolver
+  chattr +i /etc/resolv.conf #Disallow the modification of the file
+  systemctl start unbound && systemctl enable unbound #Enable named at boot and start it
 fi

1a7fe53d956a83c4df741aa035d2f29ac72e72c4
Angristan <stanislas.lange@openmailbox.org>
2015-08-15 18:53:30 +0200

    Update bind-install.sh

--- bind-install.sh
+++ bind-install.sh
@@ -2,15 +2,15 @@
 
 if [ "$UID" -ne "0" ] #User check
 then
-   echo -e "Use this script as root."
-   exit
+   echo "Use this script as root."
+   exit 1
 else
-pacman -Sy bind dnsutils
-wget ftp://ftp.internic.net/domain/named.cache -O /var/named/root.hint #Root servers list
-sed -i 's|file "/var/named/root.hint"|file "root.hint"|' /etc/named.conf
-chattr -i /etc/resolv.conf #Allow the modification of the file
-sed -i 's|nameserver|#nameserver|' /etc/resolv.conf #Disable previous DNS servers
-echo "nameserver 127.0.0.1" >> /etc/resolv.conf #Set localhost as the DNS resolver
-chattr +i /etc/resolv.conf #Disallow the modification of the file
-systemctl start named && systemctl enable named #Enable named at boot and start it
+   pacman -Sy bind dnsutils
+   wget ftp://ftp.internic.net/domain/named.cache -O /var/named/root.hint #Root servers list
+   sed -i 's|file "/var/named/root.hint"|file "root.hint"|' /etc/named.conf
+   chattr -i /etc/resolv.conf #Allow the modification of the file
+   sed -i 's|nameserver|#nameserver|' /etc/resolv.conf #Disable previous DNS servers
+   echo "nameserver 127.0.0.1" >> /etc/resolv.conf #Set localhost as the DNS resolver
+   chattr +i /etc/resolv.conf #Disallow the modification of the file
+   systemctl start named && systemctl enable named #Enable named at boot and start it
 fi

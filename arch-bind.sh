7f6dc39bdb7dc6fb7c37099769f592e538cb3d6b
Angristan <stanislas.lange@protonmail.com>
2016-12-04 00:31:39 +0100

    Update arch-bind.sh

--- arch-bind.sh
+++ arch-bind.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-if [ "$UID" -ne "0" ] #User check
-then
+if [ "$UID" -ne "0" ]; then
    echo "Use this script as root."
    exit 1
-else
-   pacman -Sy bind dnsutils
-   wget ftp://ftp.internic.net/domain/named.cache -O /var/named/root.hint #Root servers list
-   sed -i 's|file "/var/named/root.hint"|file "root.hint"|' /etc/named.conf
-   chattr -i /etc/resolv.conf #Allow the modification of the file
-   sed -i 's|nameserver|#nameserver|' /etc/resolv.conf #Disable previous DNS servers
-   echo "nameserver 127.0.0.1" >> /etc/resolv.conf #Set localhost as the DNS resolver
-   chattr +i /etc/resolv.conf #Disallow the modification of the file
-   systemctl start named && systemctl enable named #Enable named at boot and start it
-   echo "The installation is done."
 fi
+
+# Install bind
+pacman -Syu bind
+
+# Getting root servers list
+wget ftp://ftp.internic.net/domain/named.cache -O /var/named/root.hint
+sed -i 's|file "/var/named/root.hint"|file "root.hint"|' /etc/named.conf
+
+# Enable bind at boot and restart it
+systemctl restart named && systemctl enable named
+
+# Disable previous DNS servers
+sed -i 's|nameserver|#nameserver|' /etc/resolv.conf
+# Set localhost as the DNS resolver
+echo "nameserver 127.0.0.1" >> /etc/resolv.conf
+# Disallow the modification to prevent the file from being overwritten by the system
+chattr +i /etc/resolv.conf
+
+echo "The installation is done."

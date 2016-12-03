df6c3d1671a9bb374c513e19d90189bbd0b27411
Angristan <stanislas.lange@protonmail.com>
2016-12-03 19:49:34 +0100

    Create centos-unbound.sh

---
+++ centos-unbound.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [ "$UID" -ne "0" ]
+then
+	echo "Use this script as root."
+	exit 1
+else
+
+	# Install unbound
+	yum install unbound -y
+
+	# Set conf location
+	unbound -c /etc/unbound/unbound.conf
+
+	# Set root key location
+	unbound-anchor -a "/var/lib/unbound/root.key"
+	
+	# Configuration
+	echo "interface: 127.0.0.1
+access-control: 127.0.0.1 allow
+port: 53
+do-daemonize: yes
+hide-identity: yes
+hide-version: yes" >> /etc/unbound/unbound.conf
+
+	# Restart unbound
+	service unbound restart
+
+	# Allow the modification of the file
+	chattr -i /etc/resolv.conf
+	# Disable previous DNS servers
+	sed -i 's|nameserver|#nameserver|' /etc/resolv.conf
+	# Set localhost as the DNS resolver
+	echo "nameserver 127.0.0.1" >> /etc/resolv.conf
+	# Disallow the modification to prevent the file from being overwritten by the system
+	chattr +i /etc/resolv.conf
+
+	echo "The installation is done."
+fi

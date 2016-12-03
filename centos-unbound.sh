481a31d26e11cf37c974c09ceb9c48a8caebb2b1
Angristan <stanislas.lange@protonmail.com>
2016-12-03 19:50:22 +0100

    Update centos-unbound.sh

--- centos-unbound.sh
+++ centos-unbound.sh
@@ -1,39 +1,37 @@
 #!/bin/bash
 
-if [ "$UID" -ne "0" ]
-then
+if [ "$UID" -ne "0" ]; then
 	echo "Use this script as root."
 	exit 1
-else
+fi
 
-	# Install unbound
-	yum install unbound -y
+# Install unbound
+yum install unbound -y
 
-	# Set conf location
-	unbound -c /etc/unbound/unbound.conf
+# Set conf location
+unbound -c /etc/unbound/unbound.conf
 
-	# Set root key location
-	unbound-anchor -a "/var/lib/unbound/root.key"
+# Set root key location
+unbound-anchor -a "/var/lib/unbound/root.key"
 	
-	# Configuration
-	echo "interface: 127.0.0.1
+# Configuration
+echo "interface: 127.0.0.1
 access-control: 127.0.0.1 allow
 port: 53
 do-daemonize: yes
 hide-identity: yes
 hide-version: yes" >> /etc/unbound/unbound.conf
 
-	# Restart unbound
-	service unbound restart
+# Restart unbound
+service unbound restart
 
-	# Allow the modification of the file
-	chattr -i /etc/resolv.conf
-	# Disable previous DNS servers
-	sed -i 's|nameserver|#nameserver|' /etc/resolv.conf
-	# Set localhost as the DNS resolver
-	echo "nameserver 127.0.0.1" >> /etc/resolv.conf
-	# Disallow the modification to prevent the file from being overwritten by the system
-	chattr +i /etc/resolv.conf
+# Allow the modification of the file
+chattr -i /etc/resolv.conf
+# Disable previous DNS servers
+sed -i 's|nameserver|#nameserver|' /etc/resolv.conf
+# Set localhost as the DNS resolver
+echo "nameserver 127.0.0.1" >> /etc/resolv.conf
+# Disallow the modification to prevent the file from being overwritten by the system
+chattr +i /etc/resolv.conf
 
-	echo "The installation is done."
-fi
+echo "The installation is done."

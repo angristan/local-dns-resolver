86f2850c05ae40e392decb3f06431adc79f53c57
Angristan <stanislas.lange@protonmail.com>
2016-12-04 01:12:24 +0100

    Update arch-unbound.sh

--- arch-unbound.sh
+++ arch-unbound.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-if [ "$UID" -ne "0" ] #User check
-then
-  echo "Use this script as root."
-  exit 1
-else
-  pacman -Sy unbound expat
-  wget ftp://FTP.INTERNIC.NET/domain/named.cache -O /etc/unbound/root.hints #Root servers list
-  chmod 666 /etc/unbound/unbound.conf
-  echo 'server:
+if [ "$UID" -ne "0" ]; then
+	echo "Use this script as root."
+	exit 1
+fi
+
+# Install unbound
+pacman -Sy unbound expat
+
+# Get root servers list
+wget ftp://FTP.INTERNIC.NET/domain/named.cache -O /etc/unbound/root.hints #Root servers list
+chmod 666 /etc/unbound/unbound.conf
+
+# Configuration
+mv /etc/unbound/unbound.conf /etc/unbound/unbound.conf.old
+echo 'server:
 root-hints: "/etc/unbound/root.hints"
 interface: 127.0.0.1
 access-control: 127.0.0.1 allow
@@ -19,11 +25,18 @@ use-caps-for-id: yes
 harden-glue: yes
 hide-identity: yes
 hide-version: yes
-qname-minimisation: yes' >> /etc/unbound/unbound.conf
-  chattr -i /etc/resolv.conf #Allow the modification of the file
-  sed -i 's|nameserver|#nameserver|' /etc/resolv.conf #Disable previous DNS servers
-  echo "nameserver 127.0.0.1" >> /etc/resolv.conf #Set localhost as the DNS resolver
-  chattr +i /etc/resolv.conf #Disallow the modification of the file
-  systemctl start unbound && systemctl enable unbound #Enable named at boot and start it
-  echo "The installation is done."
-fi
+qname-minimisation: yes' > /etc/unbound/unbound.conf
+
+# Enable unbound at boot and restart it
+systemctl restart unbound && systemctl enable unbound
+
+# Allow the modification of the file
+chattr -i /etc/resolv.conf
+# Disable previous DNS servers
+sed -i 's|nameserver|#nameserver|' /etc/resolv.conf
+# Set localhost as the DNS resolver
+echo "nameserver 127.0.0.1" >> /etc/resolv.conf
+# Disallow the modification to prevent the file from being overwritten by the system
+chattr +i /etc/resolv.conf
+
+echo "The installation is done."

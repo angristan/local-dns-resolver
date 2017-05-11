04aa56444f8d48f30bbaeaff764ec43e9546f4d4
Angristan <stanislas.lange@protonmail.com>
2017-05-11 19:19:55 +0200

    Move root servers list

--- centos-unbound.sh
+++ centos-unbound.sh
@@ -15,12 +15,13 @@ unbound -c /etc/unbound/unbound.conf
 unbound-anchor -a "/var/lib/unbound/root.key"
 
 # Get root servers list
-wget ftp://FTP.INTERNIC.NET/domain/named.cache -O /etc/unbound/root.hints
+wget ftp://FTP.INTERNIC.NET/domain/named.cache -O /var/lib/unbound/root.hints
 	
 # Configuration
 mv /etc/unbound/unbound.conf /etc/unbound/unbound.conf.old
+
 echo "server:
-root-hints: "/etc/unbound/root.hints"
+root-hints: "root-hints: "/var/lib/unbound/root.hints"
 auto-trust-anchor-file: "/var/lib/unbound/root.key"
 interface: 127.0.0.1
 access-control: 127.0.0.1 allow

d0ceb2a1d8a1d64d559692c6ca4191ea73a8cc30
Angristan <stanislas.lange@protonmail.com>
2017-08-07 16:33:02 +0200

    Cleanup

--- debian-unbound.sh
+++ debian-unbound.sh
@@ -8,9 +8,6 @@ fi
 # Install unbound
 apt install -y unbound
 
-# Set conf location
-unbound -c /etc/unbound/unbound.conf
-
 # Set root key location (for DNSSEC)
 unbound-anchor -a "/var/lib/unbound/root.key"
 
@@ -20,9 +17,9 @@ wget ftp://FTP.INTERNIC.NET/domain/named.cache -O /var/lib/unbound/root.hints
 # Configuration
 mv /etc/unbound/unbound.conf /etc/unbound/unbound.conf.old
 
-echo "server:
-root-hints: "/var/lib/unbound/root.hints"
-auto-trust-anchor-file: "/var/lib/unbound/root.key"
+echo 'server:
+root-hints: /var/lib/unbound/root.hints
+auto-trust-anchor-file: /var/lib/unbound/root.key
 interface: 127.0.0.1
 access-control: 127.0.0.1 allow
 port: 53
@@ -31,7 +28,7 @@ num-threads: 2
 use-caps-for-id: yes
 harden-glue: yes
 hide-identity: yes
-hide-version: yes" > /etc/unbound/unbound.conf
+hide-version: yes' > /etc/unbound/unbound.conf
 
 # Restart unbound
 service unbound restart

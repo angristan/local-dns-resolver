b2baadd46347825cc261bd82c8a300862bc08ded
Angristan <stanislas.lange@protonmail.com>
2017-10-28 13:05:37 +0200

    Fix sed

--- fedora-unbound.sh
+++ fedora-unbound.sh
@@ -10,7 +10,7 @@ dnf install -y unbound
 
 # Configuration
 sed -i 's|# hide-identity: no|hide-identity: yes|' /etc/unbound/unbound.conf
-sed -i 's|# hide-identity: no|hide-identity: yes|' /etc/unbound/unbound.conf
+sed -i 's|# hide-version: no|hide-version: yes|' /etc/unbound/unbound.conf
 sed -i 's|# use-caps-for-id: no|use-caps-for-id: yes|' /etc/unbound/unbound.conf
 
 # Restart unbound

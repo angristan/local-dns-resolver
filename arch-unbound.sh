b590f5aadc9b2ad1561b9913d356045647d06bd4
cezar97 <34601855+cezar97@users.noreply.github.com>
2018-01-19 20:07:11 +0100

    Update unbound config

--- arch-unbound.sh
+++ arch-unbound.sh
@@ -17,18 +17,35 @@ wget ftp://FTP.INTERNIC.NET/domain/named.cache -O /etc/unbound/root.hints
 
 # Configuration
 mv /etc/unbound/unbound.conf /etc/unbound/unbound.conf.old
-echo 'server:
-root-hints: root.hints
-auto-trust-anchor-file: trusted-key.key
+echo '### Based on http://calomel.org/unbound_dns.html ###
+server:
 interface: 127.0.0.1
-access-control: 127.0.0.1 allow
 port: 53
+do-ip4: yes
+do-ip6: no
+do-udp: yes
+do-tcp: yes
 do-daemonize: yes
-num-threads: 2
-use-caps-for-id: yes
-harden-glue: yes
+access-control: 127.0.0.1 allow
+root-hints: "/etc/unbound/root.hints"
+auto-trust-anchor-file: "/etc/unbound/root.key"
 hide-identity: yes
 hide-version: yes
+harden-glue: yes
+harden-dnssec-stripped: yes
+use-caps-for-id: yes
+cache-min-ttl: 3600
+cache-max-ttl: 86400
+prefetch: yes
+#num-threads: 2
+rrset-cache-size: 128m
+msg-cache-size: 64m
+so-rcvbuf: 1m
+private-address: 192.168.0.0/16
+private-address: 172.16.0.0/12
+private-address: 10.0.0.0/8
+unwanted-reply-threshold: 10000
+val-clean-additional: yes
 qname-minimisation: yes' > /etc/unbound/unbound.conf
 
 # Enable unbound at boot and restart it
